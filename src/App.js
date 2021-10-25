795a860170a5b3cad281a966c4620f524109ab49
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2021-10-25 21:08:10 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My todo</h1>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+function Todo(props) {
+    return(
+        <div className='card'>
+            <h1>{props.text}</h1>
+            <div className='actions'>
+                <button className='btn'>Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo;
